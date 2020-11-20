84ef85fa367e89907f58cd3f93794c47eb02d918
LIA <lvxing29@naver.com>
2020-11-20 16:45:16 +0900

    routing2

--- route/order.js
+++ route/order.js
@@ -3,17 +3,33 @@ const express = require("express")
 const router = express.Router()
 
 
-
+// order 불러오는 API
 router.get("/2222", (req, res) => {
     res.json( {
         message : "order get"
     })
 })
 
+// order 등록해주는 API
+router.post("/",(req, res) => {
+    res.json({
+        msg: "order등록해주는 API"
+    })
+})
+
+// order 수정하는 API
+router.patch("/", (req, res) => {
+    res.json({
+        msg:"order수정하는 API"
+    })
+})
 
 
-
-
-
+// order 삭제하는 API
+router.delete("/", (req, res) =>{
+    res.json({
+        msg:"order 삭제하는 api"
+    })
+})
 
 module.exports = router
--- route/product.js
+++ route/product.js
@@ -3,6 +3,8 @@ const express = require("express")
 const router = express.Router()
 
 // 3
+
+// product 불러오는 API
 router.get("/1111", (req, res) => {
     res.json({
         message : "ㅇㅏㅁㅜㄱㅓㄴㅏ"
@@ -10,6 +12,26 @@ router.get("/1111", (req, res) => {
 })
 
 
+// product 등록해주는 API
+router.post("/",(req, res) => {
+    res.json({
+        msg: "product 등록해주는 API"
+    })
+})
+
+// product 수정하는 API
+router.patch("/", (req, res) => {
+    res.json( {
+        msg:"product 수정주는 API"
+    })
+})
+
+// product 삭제하는 API
+router.delete("/", (req, res) => {
+    res.json({
+        msg :"product 삭제해주는 API"
+    })
+})
 
 
 
