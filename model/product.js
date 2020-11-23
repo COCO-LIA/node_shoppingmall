095a923152367e4926d092037183100e7c062c01
LIA <lvxing29@naver.com>
2020-11-23 10:37:09 +0900

    Add product model

---
+++ model/product.js
@@ -0,0 +1,16 @@
+//1
+const mongoose = require('mongoose')
+
+//2
+const productSchema = mongoose.Schema({
+    name : String,
+    price : Number,
+    category: String
+})
+
+
+
+// 3
+
+module.exports = mongoose.model("product", productSchema)
+
