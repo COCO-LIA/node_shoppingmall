a15aa83a95b8e8b78080cad7bdc937f64326fc6f
LIA <lvxing29@naver.com>
2020-11-28 15:30:41 +0900

    회원가입#1

--- model/user.js
+++ model/user.js
@@ -4,7 +4,20 @@ const mongoose = require('mongoose')
 
 //2
 
-const userSchema = mongoose.Schema({})
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+})
 
 //3
 
--- route/user.js
+++ route/user.js
@@ -3,8 +3,44 @@
 const express = require('express')
 const router = express.Router()
 
-//3 API
+const userModel = require('../model/user')
 
+//3 API
+//회원가입 API
+router.post("/register", (req, res) => {
+
+    const  userInfo = new  userModel({
+        username:req.body.usn,
+        email:req.body.em,
+        password:req.body.paw
+
+    })
+
+    userInfo
+        .save()
+        .then(user => {
+            res.json({
+                msg: " 회원가입 됐습니다 ",
+                userInfo: user
+
+            })
+        })
+        .catch(err => {
+            res.json({
+                msg: err.message
+            })
+        })
+
+
+})
+
+
+
+
+// 로그인 API
+router.post("/login", (req, res) => {
+
+})
 
 //2
 
