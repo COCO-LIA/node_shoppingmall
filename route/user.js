e5ee9b2282641eff9d82e00f0a73a33ce79fb7f0
LIA <lvxing29@naver.com>
2020-11-28 15:47:23 +0900

    이메일 중복체크

--- route/user.js
+++ route/user.js
@@ -10,37 +10,61 @@ const userModel = require('../model/user')
 //회원가입 API
 router.post("/register", (req, res) => {
 
-    bcrypt.hash(req.body.paw, 10, (err, hash) => {
-
-        if(err) {
-            return res.json({
-                error: err
-            })
-        }
-        else {
-            const  userInfo = new  userModel({
-                username:req.body.usn,
-                email:req.body.em,
-                password:hash
-
-            })
-
-            userInfo
-                .save()
-                .then(user => {
-                    res.json({
-                        msg: " 회원가입 됐습니다 ",
-                        userInfo: user
-
-                    })
+    // email 유무체크 -> 패스워드 암호화 -> 데이터베이스에 유저정보 저장
+    userModel
+        .findOne({email: req.body.em})
+        .then(user => {
+            if(user) {
+                return res.json({
+                    msg: "다른 이메일로 회원가입 해주십시오."
                 })
-                .catch(err => {
-                    res.json({
-                        msg: err.message
-                    })
+            } else {
+                bcrypt.hash(req.body.paw, 10, (err, hash) => {
+
+                    if(err) {
+                        return res.json({
+                            error: err
+                        })
+                    }
+                    else {
+                        const  userInfo = new  userModel({
+                            username:req.body.usn,
+                            email:req.body.em,
+                            password:hash
+
+                        })
+
+                        userInfo
+                            .save()
+                            .then(user => {
+                                res.json({
+                                    msg: " 회원가입 됐습니다 ",
+                                    userInfo: user
+
+                                })
+                            })
+                            .catch(err => {
+                                res.json({
+                                    msg: err.message
+                                })
+                            })
+                    }
                 })
-        }
-    })
+
+            }
+        })
+        .catch(err => {
+            res.json({
+                msg: err.message
+            })
+        })
+
+
+
+
+
+    //
+
 
 
 
