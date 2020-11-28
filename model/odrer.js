fefe977aac154eb7613e61317808acafc1180a14
LIA <lvxing29@naver.com>
2020-11-28 14:31:23 +0900

    Add order model

--- model/odrer.js
+++ model/odrer.js
@@ -6,7 +6,17 @@ const mongoose = require('mongoose')
 
 //2
 
-const orderSchema = mongoose.Schema()
+const orderSchema = mongoose.Schema({
+    product:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'product',
+        required: true
+    },
+    quantity:{
+        type:Number,
+        default: 1
+    }
+})
 
 //3
 module.exports = mongoose.model("order", orderSchema)
