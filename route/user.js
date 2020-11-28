3ba8cc040b6f9eda7548ed6ee7de681199beeb1f
LIA <lvxing29@naver.com>
2020-11-28 16:08:55 +0900

    로그인 프로세스

--- route/user.js
+++ route/user.js
@@ -81,6 +81,37 @@ router.post("/register", (req, res) => {
 // 로그인 API
 router.post("/login", (req, res) => {
 
+    // 이메일 유무체크 -> 패스워드 매칭-> 접속유저정보 뿌려주기 
+    userModel
+        .findOne({email: req.body.em})
+        .then(user => {
+            if(!user) {
+                return  res.json({
+                    msg: "등록되지 않은 이메일 입니다. 먼저 회원가입을 해 주십시오 "
+                })
+            } else {
+               bcrypt.compare(req.body.paw, user.password, (err, isMatch) => {
+
+                   if(err || isMatch === false){
+                       return res.json({
+                           msg: "Auth failed(password incorrected)"
+                       })
+                   } else {
+                       res.json(user)
+                   }
+               })
+
+
+            }
+        })
+        .catch(err => {
+            res.json({
+                err: err.message
+            })
+        })
+
+
+
 })
 
 //2
